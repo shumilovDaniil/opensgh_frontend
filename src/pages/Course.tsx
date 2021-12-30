913c76787ae82dd3ea766c6e3e139fbf70fc5405
Daniil <shumilov.daniil.s@gmail.com>
2021-12-31 00:12:27 +0500

    create pages/Course

---
+++ src/pages/Course.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Course: React.FC = () => {
+	return (
+		<>
+			<div className='back'>
+				<span></span>
+				<a href='#'>Elective courses</a>
+			</div>
+		</>
+	)
+}
