ba9ef22e173397af0bb64ac696867e0ed130c8fa
Daniil <shumilov.daniil.s@gmail.com>
2022-01-04 13:12:11 +0500

    add style for reviews and about

--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -221,6 +221,139 @@ export const Course: React.FC = () => {
 		padding: 16px 18px;
 		border-radius: 24px;
 		margin-bottom: 8px;
+
+		h4 {
+			font-weight: 600;
+			font-size: 20px;
+			line-height: 25px;
+			color: #001a1a;
+		}
+
+		.reviewTop {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: baseline;
+			margin-bottom: 14px;
+
+			span {
+				margin-left: 12px;
+				font-size: 14px;
+				line-height: 18px;
+				color: #686f78;
+			}
+		}
+
+		.reviewText {
+			font-size: 16px;
+			line-height: 23px;
+			color: #002222;
+			font-weight: 400;
+			margin-bottom: 7px;
+		}
+
+		.reviewBtnWrapper {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin-bottom: 22px;
+		}
+
+		.reviewBtn {
+			background: #fff;
+			border: 1px solid #001a1a;
+			border-radius: 40px;
+			padding: 14px 45px 14px;
+			flex: 0 0 48%;
+			color: #001a1a;
+
+			&--black {
+				background: #001a1a;
+				color: #fff;
+			}
+		}
+
+		.userWrapper {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			margin-bottom: 6px;
+		}
+
+		.userCommentInfo {
+			display: flex;
+			flex-wrap: wrap;
+			align-items: flex-start;
+
+			img {
+				margin-right: 7px;
+			}
+
+			.userCommentWrapper {
+				strong {
+					font-weight: 500;
+					font-size: 14px;
+					line-height: 18px;
+					color: #001a1a;
+				}
+				span {
+					display: flex;
+					flex-wrap: wrap;
+					font-size: 11px;
+					line-height: 14px;
+					color: #686f78;
+				}
+			}
+		}
+
+		.userRating {
+			display: flex;
+			flex-wrap: wrap;
+			span {
+				font-size: 14px;
+				line-height: 18px;
+				color: #686f78;
+
+				font-weight: 500;
+
+				& + span {
+					margin-left: 12px;
+				}
+			}
+
+			.userRatingStar {
+				display: flex;
+				flex-wrap: wrap;
+				align-items: center;
+				color: #001a1a;
+			}
+		}
+
+		.commentContent {
+			font-size: 16px;
+			line-height: 23px;
+			color: #002222;
+
+			p {
+				margin-bottom: 3px;
+			}
+		}
+
+		.commentOptions {
+			display: flex;
+			flex-wrap: wrap;
+			justify-content: space-between;
+			a {
+				font-size: 14px;
+				line-height: 18px;
+				color: #686f78;
+			}
+
+			.commentRating {
+				button {
+					margin-left: 28px;
+				}
+			}
+		}
 	`
 	const OtherTeachers = styled.div`
 		background: #fff;
@@ -388,24 +521,37 @@ export const Course: React.FC = () => {
 			</SubjectAbout>
 
 			<Reviews>
-				<h4>Reviews</h4>
-				<span className='reviewsCounter'>18</span>
+				<div className='reviewTop'>
+					<h4>Reviews</h4>
+					<span className='reviewsCounter'>18</span>
+				</div>
 
 				{/* Sign/Log warning ==> Component */}
-				<strong>Log in or sign up to write a review or a comment.</strong>
-				<button>Log in</button>
-				<button>Sign up</button>
+				<strong className='reviewText'>
+					Log in or sign up to write a review or a comment.
+				</strong>
+				<div className='reviewBtnWrapper'>
+					<button className='reviewBtn reviewBtn--black'>Log in</button>
+					<button className='reviewBtn '>Sign up</button>
+				</div>
 
 				{/* Comment component */}
 
-				<div className='userCommentInfo'>
-					<img src='/images/user_photo.png' alt='User' />
-					<strong>Giorgio Z</strong>
-					<span className='userCommentTime'>50 min</span>
-				</div>
-				<div className='userRating'>
-					<span>Overall</span>
-					<span>4,5 ⭐</span>
+				<div className='userWrapper'>
+					<div className='userCommentInfo'>
+						<img src='/images/user_photo.png' alt='User' />
+						<div className='userCommentWrapper'>
+							<strong>Giorgio Z</strong>
+							<span className='userCommentTime'>50 min</span>
+						</div>
+					</div>
+					<div className='userRating'>
+						<span>Overall</span>
+						<span className='userRatingStar'>
+							4,5
+							<img src='./images/icons/star-black.svg' alt='star' />
+						</span>
+					</div>
 				</div>
 
 				<div className='commentContent'>
@@ -420,8 +566,12 @@ export const Course: React.FC = () => {
 				<div className='commentOptions'>
 					<a href='/#'>Reply</a>
 					<div className='commentRating'>
-						<button>👍</button>
-						<button>👎</button>
+						<button>
+							<img src='/images/icons/like.svg' alt='like' />8
+						</button>
+						<button>
+							<img src='/images/icons/dislike.svg' alt='dislike' />
+						</button>
 					</div>
 				</div>
 			</Reviews>
